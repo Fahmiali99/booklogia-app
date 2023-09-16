ed8689d6068054684b29ef6a29539a310cc7b283
Fahmi Ali Husni <fahmiali130499@gmail.com>
2023-09-16 22:34:20 +0700

    fix: hotfix/bug_shadow

--- src/pages/Register/index.test.js
+++ src/pages/Register/index.test.js
@@ -1,30 +1,35 @@
 import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
-import Login from "./index";
 import store from "../../redux/store";
 import { Provider } from "react-redux";
 import userEvent from "@testing-library/user-event";
+import Register from "./index";
 
 const setup = () =>
   render(
     <Provider store={store}>
-      <Login />
+      <Register />
     </Provider>
   );
 
-describe("Login Register", () => {
+describe('Testing Register', () => {
   beforeEach(setup);
   afterEach(cleanup);
 
-  it("Success render Login component", () => {
+  it("Success render Register component", () => {
+    expect(screen.getByLabelText("input-username")).toBeInTheDocument();
     expect(screen.getByLabelText("input-email")).toBeInTheDocument();
-    expect(screen.getByLabelText("input-password")).toBeInTheDocument();
+  });
+
+  it("Search username value should be same as user typing", () => {
+    const inputUsername = screen.getByLabelText("input-username");
+    userEvent.type(inputUsername, "test username");
+    expect(inputUsername.value).toBe("test username");
   });
 
   it("Search email value should be same as user typing", () => {
     const inputEmail = screen.getByLabelText("input-email");
     userEvent.type(inputEmail, "testing@gmail.com");
-
     expect(inputEmail.value).toBe("testing@gmail.com");
   });
 });
